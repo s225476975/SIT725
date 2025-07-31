25228dad9bb758c9bc7d3d9cab1a45eb659b77ce
Udith Jayasinghe <42823912+udith97@users.noreply.github.com>
2025-07-31 10:22:25 +1000

    this is week 4 mongo db workshop

---
+++ Week 4/index.js
@@ -0,0 +1,35 @@
+var express = require("express")
+var app = express()
+
+const mongoose = require('mongoose');
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb://localhost:27017/myprojectDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB!');
+});
+
+const ProjectSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    link: String,
+    description: String,
+});
+const Project = mongoose.model('Project', ProjectSchema);
+
+app.get('/api/projects', async (req, res) => {
+const projects = await Project.find({});
+res.json({ statusCode: 200, data: projects, message: "Success" });
+});
+
+var port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log("App listening to: " + port)
+})
